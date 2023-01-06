811306076ddfcdd5069a5b6b4725c651655fd18d
just3260@gmail.com <just3260@gmail.com>
2023-01-07 03:11:25 +0800

    prepare deploy on Heroku

---
+++ LinuxMain.swift
@@ -0,0 +1,13 @@
+//
+//  File.swift
+//  joja-server
+//
+//  Created by Andrew on 2023/1/7.
+//
+
+import XCTest
+@testable import MonoGeneratorTests
+
+XCTMain([
+     testCase(GeneratorTests.allTests),
+])
